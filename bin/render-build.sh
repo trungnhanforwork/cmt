ef52cd6d54b545206f256fc97ca8aaf237787edd
Trung Nhan <trungnhanforwork@gmail.com>
2025-02-24 09:59:38 +0700

    [FEAT][CMT03]: Add config to deploy.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,9 +4,6 @@ set -o errexit
 
 bundle config set --local force_ruby_platform true
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
